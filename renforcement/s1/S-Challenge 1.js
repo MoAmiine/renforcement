65d2b8215030ea43c0bd32ef6ba125d65bb0a2e3
Mohamed amine TELJAOUI <93794412+MoAmiine@users.noreply.github.com>
2026-03-18 14:10:40 +0000

    semaine 1 challenge 1

---
+++ renforcement/s1/S-Challenge 1.js
@@ -0,0 +1,9 @@
+let participants = ['amine', 'mouad', 'aymen', 'abdellah', 'mohamed', 'teljaoui'];
+
+console.log(participants);
+console.log(participants[0]);
+console.log(participants[participants.length - 1]);
+participants.push('roronoa', 'sasuke');
+console.log(participants.pop());
+participants.shift();
+console.log(participants);
