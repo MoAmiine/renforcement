dc12d1f7b223e76e662d50eb1ab5b2cf89e468af
Mohamed amine TELJAOUI <93794412+MoAmiine@users.noreply.github.com>
2026-04-01 09:49:25 +0100

    1. cinq utilisateurs cree

---
+++ renforcement/s1/Mini-Projet .js
@@ -0,0 +1,43 @@
+let utilisateurs = [
+{
+id: 1,
+pseudo: "Amine",
+email: "amine@mail.com",
+role: "vendeur",
+note: 4.5,
+solde: 500
+},
+{
+id: 2,
+pseudo: "Yassine",
+email: "yassine@mail.com",
+role: "acheteur",
+note: 0,
+solde: 300
+},
+{
+id: 3,
+pseudo: "Sara",
+email: "sara@mail.com",
+role: "vendeur",
+note: 4.8,
+solde: 800
+},
+{
+id: 4,
+pseudo: "Omar",
+email: "omar@mail.com",
+role: "acheteur",
+note: 0,
+solde: 200
+},
+{
+id: 5,
+pseudo: "Lina",
+email: "lina@mail.com",
+role: "vendeur",
+note: 4.2,
+solde: 600
+}
+]
+
